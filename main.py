9cbc1068dd4e7542790aea07e662c27543e7e70c
D4niel0s <das.volkov@gmail.com>
2024-12-26 15:13:54 +0200

    Made graphs look insane and added energy/time graph

--- main.py
+++ main.py
@@ -1,69 +1,108 @@
 import librosa
+import numpy as np, matplotlib.pyplot as plt, pyworld as pw, soundfile as sf
 from scipy.signal import resample
-import numpy as np
-import matplotlib.pyplot as plt
-import pyworld as pw
-import soundfile as sf
 
-def display_audio_stats(data, sampling_rate):
-    fig, ax = plt.subplots(nrows=5, sharex=True)
-    librosa.display.waveshow(data, sr=sampling_rate, ax=ax[0])
-    ax[0].set(title='Envelope view, mono')
-    ax[0].label_outer()
+def main():
+    print("Save audio files? (Y/N)")
+    save = input()
 
-    #spektogram
-    D = librosa.stft(data)
-    magnitute = np.abs(D)
-    s_db = librosa.amplitude_to_db(magnitute, ref=np.max)
-    
-    librosa.display.specshow(s_db, sr=sampling_rate, x_axis='time', y_axis='linear', ax=ax[1], cmap='viridis')
-    
-    #pitch contour
-    data = data.astype(np.float64)
-    
-    _f0, t = pw.dio(data, sampling_rate)
-    f0 = pw.stonemask(data, _f0, t, sampling_rate)
-    ax[1].plot(t, f0, color='r')
-    data = data.astype(np.float32)
-    
-    
-    #mel spektogram
-    
-    S = librosa.feature.melspectrogram(y=data, sr= sampling_rate, n_mels=128, fmax=sampling_rate/2)
-    S_db = librosa.power_to_db(S,ref=np.max)
-    librosa.display.specshow(S_db, sr=sampling_rate, x_axis='time', y_axis='mel', ax=ax[2],fmax=sampling_rate/2
-                             ,cmap='viridis')
-    
-    #iv
-    energy = np.sum(data**2)
-    
-    rms = np.sqrt(energy/len(data))
+    y, sampling_rate = librosa.load('recording.wav', mono=True)
 
-    plt.suptitle(f'energy={energy : .3f}, rms={rms : .3f}')
+    print(f'{y.shape=}')
+    print(f'{sampling_rate=}')
+
+
+    # Resample data to 32KHz
+    new_rate = 32000
+
+    y = y.astype(np.float32)
+    number_of_samples = round(len(y) * float(new_rate) / sampling_rate)
+    data = resample(y, number_of_samples)
+
+
+    #Downsample to 16KHz using method 1
+    m1_down_sample = data[::2]
+
+
+    #Downsample to 16KHz using method 2
+    m2_down_sample = resample(data, round(len(data)*0.5))
+
+    if (save == 'Y' or save == 'y'):
+        sf.write('even_samples_resample.wav', m1_down_sample,round(new_rate/2))
+        sf.write('scipy_resampled.wav', m2_down_sample, round(new_rate/2))
+        
+    
+    graph_audio_stats(m1_down_sample, round(new_rate/2))
+    plt.suptitle("Even samples resample")
+
+    graph_audio_stats(m2_down_sample, round(new_rate/2))
+    plt.suptitle("Scipy resample")
+
     plt.show()
 
-y, sampling_rate = librosa.load('recording.wav', mono=True)
-
-print(f'{y.shape=}')
-print(f'{sampling_rate=}')
 
 
-# Resample data
-new_rate = 32000
 
-y = y.astype(np.float32)
-number_of_samples = round(len(y) * float(new_rate) / sampling_rate)
-data = resample(y, number_of_samples)
+def graph_audio_stats(data, sampling_rate):
+    window_size = round(sampling_rate * (20e-3))
+    hop_size = round(sampling_rate * (10e-3))
 
-#Method 1
-m1_down_sample = data[::2]
+    fig, ax = plt.subplots(nrows=4, sharex=True)
+    duration = len(data) / sampling_rate
 
-sf.write('manual.wav', m1_down_sample,round(new_rate/2))
+    librosa.display.waveshow(data, sr=sampling_rate, ax=ax[0])
+    ax[0].set(title='Waveform', ylabel='Pressure')
+    ax[0].label_outer() 
 
-#Method 2
-m2_down_sample = resample(data, round(len(data)*0.5))
-sf.write('libro.wav', m2_down_sample, round(new_rate/2))
+    #spectogram
+    D = librosa.stft(data, win_length=window_size, hop_length=hop_size)
+    magnitute = np.abs(D)
+    s_db = librosa.amplitude_to_db(magnitute, ref=np.max)
+    librosa.display.specshow(s_db, sr=sampling_rate,x_axis='time', y_axis='linear',win_length=window_size,hop_length=hop_size, ax=ax[1], cmap='viridis')
+    
+
+    #pitch contour
+    data = data.astype(np.float64)
+
+    _f0, t = pw.dio(data, sampling_rate)
+    f0 = (pw.stonemask(data, _f0, t, sampling_rate))
+
+    data = data.astype(np.float32)
+
+    ax[1].plot(t, f0, color='r', label='Pitch contour')
+    ax[1].set(title='Spectrogram and pitch contour')
+    ax[1].legend()
+    ax[1].label_outer()
 
     
-display_audio_stats(m2_down_sample, new_rate/2)
+
+    #mel spectogram
+    S = librosa.feature.melspectrogram(y=data, sr= sampling_rate, n_mels=128, fmax=sampling_rate/2, win_length=window_size, hop_length=hop_size)
+    S_db = librosa.power_to_db(S,ref=np.max)
+    librosa.display.specshow(S_db, sr=sampling_rate, x_axis='time', y_axis='mel', fmax=sampling_rate/2,win_length=window_size, hop_length=hop_size, ax=ax[2], cmap='viridis')
+    ax[2].set(title='Mel spectrogram')
+    ax[2].label_outer()
+
+    #Energy and RMS over time
+    energy_curve = np.array([np.sum(data[i:i+window_size]**2) for i in range(0, len(data)-window_size, hop_size)])
+    RMS_curve = np.array([np.sqrt(np.mean(data[i:i+window_size]**2)) for i in range(0, len(data)-window_size, hop_size)])
+
+
+    time_points = np.arange(len(energy_curve)) * (hop_size / sampling_rate)
+
+    ax[3].plot(time_points, energy_curve, label='Energy', color='b')
+    ax[3].plot(time_points, RMS_curve, label='RMS', color='g')
+    ax[3].set(title='Energy and RMS over time', xlabel='time')
+    ax[3].label_outer()
     
+
+    #For some reason the x axis is not being set correctly, this is a workaround
+    for axes in ax:
+        axes.set_xlim(0, duration)
+    
+
+    plt.subplots_adjust(hspace=1)
+
+
+if __name__ == '__main__':
+    main()
